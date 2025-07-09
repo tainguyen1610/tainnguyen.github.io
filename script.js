7ded48b9f46ae15c3e31a37c3bf13f309d1b2bc9
Nguyễn Tấn Tài <nguyentaitan161005@gmail.com>
2025-07-09 15:49:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,17 @@ new Swiper('.card-wrapper', {
     loop: true,
     spaceBetween: 30,
 
-    // Pagination bullets
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
         dynamicBullets: true
     },
 
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
 
-    // Responsive breakpoints
     breakpoints: {
         0: {
             slidesPerView: 1
